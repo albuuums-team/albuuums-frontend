553a0428a658409e9e1f42bf153190224e68528b
Maryess <vip.ivan.ivanov2003@bk.ru>
2024-03-21 13:36:13 +0300

    change input-component

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,13 +1,12 @@
-import IconSearch from '@/shared/ui-kit/icon/ui/icons'
 import Input from '@/shared/ui-kit/input/ui/input'
 import { IoSearch } from 'react-icons/io5'
-import style from '../../../shared/ui-kit/input/ui/input.module.css'
+
 export const HomePage = () => {
 	return (
 		<>
-			<Input placeholder='Поиск пользавателя' />
-			<IconSearch
-				icon={props => <IoSearch className={style['icon']} {...props} />}
+			<Input
+				placeholder='Поиск пользавателя'
+				icon={props => <IoSearch {...props} />}
 			/>
 		</>
 	)
--- src/shared/ui-kit/input/ui/input.tsx
+++ src/shared/ui-kit/input/ui/input.tsx
@@ -1,14 +1,25 @@
-import { FunctionComponent, InputHTMLAttributes } from 'react'
+import React, { FunctionComponent, InputHTMLAttributes } from 'react'
 import style from './input.module.css'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
 	placeholder: string
 }
 
+interface IconTypeProps {
+	width: number
+	height: number
+}
+
+type IconType = (props: IconTypeProps) => JSX.Element
+
+interface Props {
+	icon: IconType
+}
+
 const Input: FunctionComponent<Props> = ({
 	placeholder,
 	children,
-
+	icon,
 	...rest
 }) => {
 	return (
@@ -19,6 +30,9 @@ const Input: FunctionComponent<Props> = ({
 				placeholder={placeholder}
 				{...rest}
 			/>
+			<div className={style.icon}>
+				{React.createElement(icon, { width: 20, height: 20 })}
+			</div>
 		</div>
 	)
 }
